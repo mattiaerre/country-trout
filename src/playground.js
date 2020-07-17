f8274b299335d6edfa157024dae7b144b35254f7
mattia richetto <mattia.richetto@gmail.com>
2020-07-17 09:51:10 -0700

    add description

--- src/playground.js
+++ src/playground.js
@@ -1,5 +1,13 @@
 const slugify = require('slugify');
 
+const recipes = require('./recipes.json');
+
 // console.log(slugify('Risotto al salmone', { lower: true }));
 
 console.log(slugify('Butternut Squash Lasagna', { lower: true }));
+
+const _ = Object.keys(recipes).reduce((accumulator, current) => {
+  return new Set([...accumulator, ...recipes[current].tags]);
+}, new Set());
+
+console.log(Array.from(_).sort());
